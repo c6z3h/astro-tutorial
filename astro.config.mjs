2b680a2a2cdf8bae0d7529fa3a3c4e10818b3a69
c6z3h <chuazhihong@hotmail.com>
2025-05-23 00:00:03 +0800

    Add simple hydration (astro islands)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://c6z3h.github.io',
+    base: '/astro-tutorial',
+    integrations: [mdx(), sitemap(), preact()],
 });
